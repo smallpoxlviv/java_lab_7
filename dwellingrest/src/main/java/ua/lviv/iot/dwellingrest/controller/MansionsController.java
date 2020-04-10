892f9fcd688239afea771dce77ce557ee19e607c
smallpoxlviv <57070960+smallpoxlviv@users.noreply.github.com>
2020-04-10 17:45:22 +0300

    Update MansionsController.java

--- dwellingrest/src/main/java/ua/lviv/iot/dwellingrest/controller/MansionsController.java
+++ dwellingrest/src/main/java/ua/lviv/iot/dwellingrest/controller/MansionsController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.springframework.http.HttpStatus;
